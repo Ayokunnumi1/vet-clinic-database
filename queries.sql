4988169a3b189b32358d0638fbdd150da96948ee
Ayokunnumi1 <omololuayk@gmail.com>
2024-09-02 15:27:18 +0100

    Add 2 more queries for animals table

--- queries.sql
+++ queries.sql
@@ -1,3 +1,14 @@
 /*Queries that provide answers to the questions from all projects.*/
 
-SELECT * from animals WHERE name = 'Luna';
+SELECT * FROM animals WHERE name LIKE '%mon';
+
+SELECT name
+FROM animals
+WHERE
+    date_of_birth BETWEEN '2016-01-01' AND '2019-01-01';
+
+SELECT name
+FROM animals
+WHERE
+    neutered = true
+    AND escape_attempts < 3;
