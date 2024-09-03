6e736928f5459c74600a45ac8867c3aa38764124
Ayokunnumi1 <omololuayk@gmail.com>
2024-09-03 01:52:35 +0100

    Modify your inserted animals to include owner information (owner_id)

--- data.sql
+++ data.sql
@@ -35,6 +35,73 @@ VALUES (
         11
     );
 
+
+INSERT INTO
+    animals (
+        id,
+        name,
+        date_of_birth,
+        escape_attempts,
+        neutered,
+        weight_kg
+    )
+VALUES (
+        5,
+        'Charmander',
+        '2020-02-08',
+        0,
+        false,
+        -11
+    ),
+    (
+        6,
+        'Plantmon',
+        '2021-11-15',
+        2,
+        true,
+        -5.7
+    ),
+    (
+        7,
+        'Squirtle',
+        '1993-04-02',
+        3,
+        false,
+        -12.13
+    ),
+    (
+        8,
+        'Angemon',
+        '2005-06-12',
+        1,
+        true,
+        -45
+    ),
+    (
+        9,
+        'Boarmon',
+        '2005-06-07',
+        7,
+        true,
+        20.4
+    ),
+    (
+        10,
+        'Blossom',
+        '1998-10-13',
+        3,
+        true,
+        17
+    ),
+    (
+        11,
+        'Ditto',
+        '2022-05-14',
+        4,
+        true,
+        22
+    );
+
 INSERT INTO
     owners (id, full_name, age)
 VALUES (1, 'Sam Smith', 34),
@@ -58,3 +125,34 @@ WHERE Not name Like '%mon';
 UPDATE animals
 SET species_id = 2
 WHERE name Like '%mon';
+
+-- Modify your inserted animals to include owner information (owner_id):
+UPDATE animals SET owner_id = 1 WHERE name = 'Agumon';
+UPDATE animals
+SET
+    owner_id = 2
+WHERE
+    name = 'Gabumon'
+    OR name = 'Pikachu';
+
+UPDATE animals
+SET
+    owner_id = 3
+WHERE
+    name = 'Devimon'
+    OR name = 'Plantmon';
+
+UPDATE animals
+SET
+    owner_id = 4
+WHERE
+    name = 'Charmander'
+    OR name = 'Squirtle'
+    OR name = 'Blossom';
+
+UPDATE animals
+SET
+    owner_id = 5
+WHERE
+    name = 'Angemon'
+    OR name = 'Boarmon';
