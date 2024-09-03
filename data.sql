917b5ad45aac8df368a8f059c9cc1f172d21df23
Ayokunnumi1 <omololuayk@gmail.com>
2024-09-03 01:25:15 +0100

    Modify inserted animals so it includes the species_id value

--- data.sql
+++ data.sql
@@ -34,3 +34,27 @@ VALUES (
         true,
         11
     );
+
+INSERT INTO
+    owners (id, full_name, age)
+VALUES (1, 'Sam Smith', 34),
+    (2, 'Jennifer Orwell', 19),
+    (3, 'Bob', 45),
+    (4, 'Melody Pond', 77),
+    (5, 'Dean Winchester', 14),
+    (6, 'Jodie Whittaker', 38);
+
+-- Insert the following data into the species table
+Insert into species Values (1, 'Pokemon'), (2, 'Digimon');
+
+-- Modify your inserted animals so it includes the species_id value:
+-- If the name ends in "mon" it will be Digimon
+-- All other animals are Pokemon
+
+UPDATE animals
+SET species_id = 1
+WHERE Not name Like '%mon';
+
+UPDATE animals
+SET species_id = 2
+WHERE name Like '%mon';
