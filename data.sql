00798ee3f0df8ab75f86531df80760c6cb824e7a
Ayokunnumi1 <omololuayk@gmail.com>
2024-09-03 15:57:40 +0100

    Restructure table so that id is set to autoincrement

--- data.sql
+++ data.sql
@@ -1,44 +1,7 @@
 /* Populate database with sample data. */
 
 INSERT INTO
-    animals
-VALUES (
-        1,
-        'Agumon',
-        '2020-02-03',
-        0,
-        true,
-        10.23
-    ),
     animals (
-        2,
-        'Gabumon',
-        '2018-11-15',
-        2,
-        true,
-        8
-    ),
-    (
-        3,
-        'Pikachu',
-        '2021-01-7',
-        1,
-        false,
-        15.04
-    ),
-    (
-        4,
-        'Devimon',
-        '2017-05-12',
-        5,
-        true,
-        11
-    );
-
-
-INSERT INTO
-    animals (
-        id,
         name,
         date_of_birth,
         escape_attempts,
@@ -46,7 +9,43 @@ INSERT INTO
         weight_kg
     )
 VALUES (
+        'Agumon',
+        '2020-02-03',
+        0,
+        true,
+        10.23
+    ),
+    (
+        'Gabumon',
+        '2018-11-15',
+        2,
+        true,
+        8
+    ),
+    (
+        'Pikachu',
+        '2021-01-7',
+        1,
+        false,
+        15.04
+    ),
+    (
+        'Devimon',
+        '2017-05-12',
         5,
+        true,
+        11
+    );
+
+INSERT INTO
+    animals (
+        name,
+        date_of_birth,
+        escape_attempts,
+        neutered,
+        weight_kg
+    )
+VALUES (
         'Charmander',
         '2020-02-08',
         0,
@@ -54,7 +53,6 @@ VALUES (
         -11
     ),
     (
-        6,
         'Plantmon',
         '2021-11-15',
         2,
@@ -62,7 +60,6 @@ VALUES (
         -5.7
     ),
     (
-        7,
         'Squirtle',
         '1993-04-02',
         3,
@@ -70,7 +67,6 @@ VALUES (
         -12.13
     ),
     (
-        8,
         'Angemon',
         '2005-06-12',
         1,
@@ -78,7 +74,6 @@ VALUES (
         -45
     ),
     (
-        9,
         'Boarmon',
         '2005-06-07',
         7,
@@ -86,7 +81,6 @@ VALUES (
         20.4
     ),
     (
-        10,
         'Blossom',
         '1998-10-13',
         3,
@@ -94,7 +88,6 @@ VALUES (
         17
     ),
     (
-        11,
         'Ditto',
         '2022-05-14',
         4,
@@ -103,13 +96,13 @@ VALUES (
     );
 
 INSERT INTO
-    owners (id, full_name, age)
-VALUES (1, 'Sam Smith', 34),
-    (2, 'Jennifer Orwell', 19),
-    (3, 'Bob', 45),
-    (4, 'Melody Pond', 77),
-    (5, 'Dean Winchester', 14),
-    (6, 'Jodie Whittaker', 38);
+    owners (full_name, age)
+VALUES ('Sam Smith', 34),
+    ('Jennifer Orwell', 19),
+    ('Bob', 45),
+    ('Melody Pond', 77),
+    ('Dean Winchester', 14),
+    ('Jodie Whittaker', 38);
 
 -- Insert the following data into the species table
 Insert into species Values (1, 'Pokemon'), (2, 'Digimon');
@@ -118,16 +111,13 @@ Insert into species Values (1, 'Pokemon'), (2, 'Digimon');
 -- If the name ends in "mon" it will be Digimon
 -- All other animals are Pokemon
 
-UPDATE animals
-SET species_id = 1
-WHERE Not name Like '%mon';
+UPDATE animals SET species_id = 1 WHERE Not name Like '%mon';
 
-UPDATE animals
-SET species_id = 2
-WHERE name Like '%mon';
+UPDATE animals SET species_id = 2 WHERE name Like '%mon';
 
 -- Modify your inserted animals to include owner information (owner_id):
 UPDATE animals SET owner_id = 1 WHERE name = 'Agumon';
+
 UPDATE animals
 SET
     owner_id = 2
